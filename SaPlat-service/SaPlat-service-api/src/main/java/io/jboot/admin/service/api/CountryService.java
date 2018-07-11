475393e3ec63e8a99e854dd56722e242ee65aa20
jywei2016 <254637046@qq.com>
2018-07-11 12:10:26 +0800

    1. 修改项目css样式，以防登录等页面太丑 2. 修改vip_nav.js，使得登录之后的左侧菜单默认为第一组菜单为展开状态，其他菜单为折叠状态 3. 增加国家管理模块

--- SaPlat-service/SaPlat-service-api/src/main/java/io/jboot/admin/service/api/CountryService.java
+++ SaPlat-service/SaPlat-service-api/src/main/java/io/jboot/admin/service/api/CountryService.java
@@ -24,6 +24,28 @@ public interface CountryService  {
      */
     public List<Country> findAll();
 
+    /**
+     * 分页查询 项目阶段 信息
+     * @param model 项目阶段
+     * @return 页
+     */
+    public Page<Country> findPage(Country model, int pageNumber, int pageSize);
+
+    /**
+     * 根据名称查询 项目阶段 信息
+     * @param name
+     * @return
+     */
+    public Country findByName(String name);
+
+
+    /**
+     * 项目阶段 是否存在
+     * @param name
+     * @return 存在返回-true，否则返回false
+     */
+    public boolean isExisted(String name);
+
 
     /**
      * delete model by primary key
---
+++ SaPlat/src/main/java/io/jboot/admin/controller/app/CountryController.java
@@ -0,0 +1,142 @@
+package io.jboot.admin.controller.app;
+
+import com.jfinal.plugin.activerecord.Page;
+import io.jboot.admin.base.common.RestResult;
+import io.jboot.admin.base.exception.BusinessException;
+import io.jboot.admin.base.interceptor.NotNullPara;
+import io.jboot.admin.base.rest.datatable.DataTable;
+import io.jboot.admin.base.web.base.BaseController;
+import io.jboot.admin.service.api.CountryService;
+import io.jboot.admin.service.entity.model.Country;
+import io.jboot.admin.service.entity.status.system.DataStatus;
+import io.jboot.core.rpc.annotation.JbootrpcService;
+import io.jboot.web.controller.annotation.RequestMapping;
+
+import java.util.Date;
+
+/**
+ * -----------------------------
+ *
+ * @author JiayinWei
+ * @version 2.0
+ * -----------------------------
+ * @date 10:05 2018/7/2
+ */
+@RequestMapping("/app/country")
+public class CountryController extends BaseController{
+
+    @JbootrpcService
+    private CountryService countryService;
+
+    /**
+     * index
+     */
+    public void index() {
+        render("main.html");
+    }
+    /**
+     * res表格数据
+     */
+    public void tableData() {
+        int pageNumber = getParaToInt("pageNumber", 1);
+        int pageSize = getParaToInt("pageSize", 30);
+
+        Country model = new Country();
+        model.setName(getPara("name"));
+
+        Page<Country> page = countryService.findPage(model, pageNumber, pageSize);
+
+        renderJson(new DataTable<Country>(page));
+    }
+
+    /**
+     * delete
+     */
+    public void delete(){
+        Long id = getParaToLong("id");
+        if (!countryService.deleteById(id)){
+            throw new BusinessException("删除失败");
+        }
+        renderJson(RestResult.buildSuccess());
+    }
+
+    @NotNullPara({"id"})
+    public void update(){
+        Long id = getParaToLong("id");
+        Country model = countryService.findById(id);
+        setAttr("model", model).render("update.html");
+    }
+
+    public void add(){
+        render("add.html");
+    }
+
+    public void postAdd(){
+        Country model = getBean(Country.class, "model");
+        if (countryService.isExisted(model.getName())){
+            throw new BusinessException("所指定的项目阶段名称已存在");
+        }
+        model.setIsEnable(true);
+        if (!countryService.save(model)){
+            throw new BusinessException("保存失败");
+        }
+        renderJson(RestResult.buildSuccess());
+    }
+
+    public void postUpdate(){
+        Country model = getBean(Country.class, "model");
+        Country byId = countryService.findById(model.getId());
+        if (byId == null){
+            throw new BusinessException("所指定的项目阶段名称不存在");
+        }
+        if (!countryService.update(model)){
+            throw new BusinessException("修改失败");
+        }
+        renderJson(RestResult.buildSuccess());
+    }
+    /**
+     * 启用项目阶段
+     */
+    @NotNullPara({"id"})
+    public void use() {
+        Long id = getParaToLong("id");
+
+        Country model = countryService.findById(id);
+        if (model == null) {
+            throw new BusinessException("对象不存在");
+        }
+
+        model.setStatus(DataStatus.USED);
+        model.setLastAccessTime(new Date());
+        model.setRemark("启用对象");
+
+        if (!countryService.update(model)) {
+            throw new BusinessException("启用失败");
+        }
+
+        renderJson(RestResult.buildSuccess());
+    }
+
+    /**
+     * 禁用项目阶段
+     */
+    @NotNullPara({"id"})
+    public void unuse() {
+        Long id = getParaToLong("id");
+
+        Country model = countryService.findById(id);
+        if (model == null) {
+            throw new BusinessException("对象不存在");
+        }
+
+        model.setStatus(DataStatus.UNUSED);
+        model.setLastAccessTime(new Date());
+        model.setRemark("禁用对象");
+
+        if (!countryService.update(model)) {
+            throw new BusinessException("禁用失败");
+        }
+
+        renderJson(RestResult.buildSuccess());
+    }
+}
