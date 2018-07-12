ef9ee204d23e7e756a85794a4b623d99cbd1cfc5
Liuchuanjin <chuanjin_Liu@163.com>
2018-07-12 11:54:00 +0800

    服务机构实现类更新

--- SaPlat-service/SaPlat-service-provider/src/main/java/io/jboot/admin/service/provider/FacAgencyServiceImpl.java
+++ SaPlat-service/SaPlat-service-provider/src/main/java/io/jboot/admin/service/provider/FacAgencyServiceImpl.java
@@ -1,10 +1,12 @@
 package io.jboot.admin.service.provider;
 
+import com.jfinal.kit.StrKit;
+import com.jfinal.plugin.activerecord.Page;
 import io.jboot.admin.service.api.FacAgencyService;
 import io.jboot.admin.service.entity.model.FacAgency;
-import io.jboot.admin.service.entity.model.Management;
 import io.jboot.aop.annotation.Bean;
 import io.jboot.core.rpc.annotation.JbootrpcService;
+import io.jboot.db.model.Columns;
 import io.jboot.service.JbootServiceBase;
 
 import javax.inject.Singleton;
@@ -16,5 +18,24 @@ public class FacAgencyServiceImpl extends JbootServiceBase<FacAgency> implements
     @Override
     public FacAgency findByOrgID(Long orgID) {
         return DAO.findFirstByColumn("orgID", orgID);
+    }
+
+    @Override
+    public Page<FacAgency> findPage(FacAgency model, int pageNumber, int pageSize) {
+        Columns columns = Columns.create();
+        if (StrKit.notBlank(model.getName())) {
+            columns.like("name", "%" + model.getName() + "%");
+        }
+        return DAO.paginateByColumns(pageNumber, pageSize, columns.getList(), "id desc");
+    }
+
+    @Override
+    public boolean isExisted(String name) {
+        return findByName(name) != null;
+    }
+
+    @Override
+    public FacAgency findByName(String name) {
+        return DAO.findFirstByColumn("name", name);
     }
 }
