ad5ee86cc7ce4ddb78705a9b3079ee1001395f5b
EchoLZY <lizhongyue248@163..com>
2018-07-13 01:27:08 +0800

    1、重做专家认证模块 2、两条路线改为一条认证路线 3、修改UI界面
    Signed-off-by: EchoLZY <lizhongyue248@163..com>

---
+++ SaPlat-service/SaPlat-service-entity/src/main/java/io/jboot/admin/service/entity/status/system/AuthStatus.java
@@ -0,0 +1,43 @@
+package io.jboot.admin.service.entity.status.system;
+
+import io.jboot.admin.base.common.BaseStatus;
+
+/**
+ * -----------------------------
+ *
+ * @author EchoLZY
+ * @version 2.0
+ * -----------------------------
+ * @date 18:04 2018/7/9
+ */
+public class AuthStatus extends BaseStatus {
+    /**
+     * 未认证
+     */
+    public final static String NOT_VERIFY = "0";
+    /**
+     * 已认证
+     */
+    public final static String IS_VERIFY = "1";
+    /**
+     * 审核中
+     */
+    public final static String VERIFYING = "2";
+
+
+    public AuthStatus() {
+        add(NOT_VERIFY, "审核失败");
+        add(IS_VERIFY, "审核成功");
+        add(VERIFYING, "审核中");
+    }
+
+    private static AuthStatus me;
+
+    public static AuthStatus me() {
+        if (me == null) {
+            me = new AuthStatus();
+        }
+        return me;
+    }
+
+}
