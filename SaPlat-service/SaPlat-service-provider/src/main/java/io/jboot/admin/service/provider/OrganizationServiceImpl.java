5d5a0e62bc0f434394afb7c67900f1618160402d
rains <11945383+1782832653@users.noreply.github.com>
2018-07-12 11:20:57 +0800

    组织管理-管理员 1、实现管理员对组织的管理    管理员能够查看到并修改组织信息    可修改的信息包括 组织名称、名称全拼、组织地址、组织机构代码、负责人姓名以及负责人联系方式 2、能够查看已经提交的组织营业执照或证件文件（目前仅限图片）    组织营业执照或证件文件的上传提交操作暂未实现

--- SaPlat-service/SaPlat-service-provider/src/main/java/io/jboot/admin/service/provider/OrganizationServiceImpl.java
+++ SaPlat-service/SaPlat-service-provider/src/main/java/io/jboot/admin/service/provider/OrganizationServiceImpl.java
@@ -1,12 +1,15 @@
 package io.jboot.admin.service.provider;
 
+import com.jfinal.kit.StrKit;
 import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Page;
 import io.jboot.admin.service.api.OrganizationService;
 import io.jboot.admin.service.api.UserService;
 import io.jboot.admin.service.entity.model.Organization;
 import io.jboot.admin.service.entity.model.User;
 import io.jboot.aop.annotation.Bean;
 import io.jboot.core.rpc.annotation.JbootrpcService;
+import io.jboot.db.model.Columns;
 import io.jboot.service.JbootServiceBase;
 
 import javax.inject.Inject;
@@ -19,6 +22,21 @@ public class OrganizationServiceImpl extends JbootServiceBase<Organization> impl
 
     @Inject
     private UserService userService;
+
+    /**
+     * 分页查询
+     * @param organization 组织
+     * @param pageNumber
+     * @param pageSize
+     * @return
+     */
+    public Page<Organization> findPage(Organization organization, int pageNumber, int pageSize){
+        Columns columns = Columns.create();
+        if (StrKit.notBlank(organization.getName())){
+            columns.like("name", "%"+organization.getName()+"%");
+        }
+        return DAO.paginateByColumns(pageNumber, pageSize, columns.getList(), "id desc");
+    }
 
     @Override
     public boolean saveOrganization(Organization model, User user, Long[] roles) {
