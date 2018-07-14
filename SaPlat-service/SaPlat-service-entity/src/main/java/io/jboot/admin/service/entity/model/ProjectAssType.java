034b43f24407391d020adcd4dfcde6ee6e2e896c
jywei2016 <254637046@qq.com>
2018-07-14 11:13:17 +0800

    1. 添加项目评估类型管理模块并完善其功能

--- SaPlat-service/SaPlat-service-entity/src/main/java/io/jboot/admin/service/entity/model/ProjectAssType.java
+++ SaPlat-service/SaPlat-service-entity/src/main/java/io/jboot/admin/service/entity/model/ProjectAssType.java
@@ -1,5 +1,6 @@
 package io.jboot.admin.service.entity.model;
 
+import io.jboot.admin.service.entity.status.system.DataStatus;
 import io.jboot.db.annotation.Table;
 import io.jboot.admin.service.entity.model.base.BaseProjectAssType;
 
@@ -8,5 +9,18 @@ import io.jboot.admin.service.entity.model.base.BaseProjectAssType;
  */
 @Table(tableName = "project_ass_type", primaryKey = "id")
 public class ProjectAssType extends BaseProjectAssType<ProjectAssType> {
-	
+    public void setStatus(java.lang.String status) {
+        if (status == DataStatus.USED) {
+            setIsEnable(true);
+        } else {
+            setIsEnable(false);
+        }
+    }
+
+    public java.lang.String getStatus() {
+        if(this.getIsEnable() == true)
+            return DataStatus.USED;
+        else
+            return DataStatus.UNUSED;
+    }
 }
