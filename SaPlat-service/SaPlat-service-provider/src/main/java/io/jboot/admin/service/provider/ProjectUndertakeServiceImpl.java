3efc45d54f072a025903e0036ea4ffe978d4dcd6
EchoLZY <lizhongyue248@163..com>
2018-07-19 23:14:41 +0800

    添加承接管理模块
    Signed-off-by: EchoLZY <lizhongyue248@163..com>

--- SaPlat-service/SaPlat-service-provider/src/main/java/io/jboot/admin/service/provider/ProjectUndertakeServiceImpl.java
+++ SaPlat-service/SaPlat-service-provider/src/main/java/io/jboot/admin/service/provider/ProjectUndertakeServiceImpl.java
@@ -1,6 +1,8 @@
 package io.jboot.admin.service.provider;
 
+import com.jfinal.kit.StrKit;
 import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Page;
 import io.jboot.admin.service.api.NotificationService;
 import io.jboot.admin.service.entity.model.Notification;
 import io.jboot.aop.annotation.Bean;
@@ -20,6 +22,16 @@ public class ProjectUndertakeServiceImpl extends JbootServiceBase<ProjectUnderta
 
     @Inject
     private NotificationService notificationService;
+
+    @Override
+    public Page<ProjectUndertake> findPage(ProjectUndertake project, int pageNumber, int pageSize) {
+        Columns columns = Columns.create();
+        if (StrKit.notBlank(project.getName())){
+            columns.like("name", "%" + project.getName()+"%");
+        }
+        return DAO.paginateByColumns(pageNumber, pageSize, columns.getList(), "id desc");
+
+    }
 
     @Override
     public ProjectUndertake findByProjectIdAndFacAgencyId(Long projectId, Long facAgencyId) {
