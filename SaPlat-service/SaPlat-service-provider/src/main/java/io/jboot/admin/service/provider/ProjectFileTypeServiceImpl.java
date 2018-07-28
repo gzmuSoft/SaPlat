f6433de996071d766cb200907076ee2ad6de95a2
Rui <905153643@qq.com>
2018-07-29 00:48:45 +0800

    添加管理员项目文档管理  添加jquery-media-js
    Signed-off-by: Rui <905153643@qq.com>

--- SaPlat-service/SaPlat-service-provider/src/main/java/io/jboot/admin/service/provider/ProjectFileTypeServiceImpl.java
+++ SaPlat-service/SaPlat-service-provider/src/main/java/io/jboot/admin/service/provider/ProjectFileTypeServiceImpl.java
@@ -1,14 +1,28 @@
 package io.jboot.admin.service.provider;
 
-import io.jboot.aop.annotation.Bean;
+import com.jfinal.plugin.activerecord.Page;
 import io.jboot.admin.service.api.ProjectFileTypeService;
 import io.jboot.admin.service.entity.model.ProjectFileType;
+import io.jboot.aop.annotation.Bean;
+import io.jboot.core.rpc.annotation.JbootrpcService;
+import io.jboot.db.model.Columns;
 import io.jboot.service.JbootServiceBase;
 
 import javax.inject.Singleton;
 
 @Bean
 @Singleton
+@JbootrpcService
 public class ProjectFileTypeServiceImpl extends JbootServiceBase<ProjectFileType> implements ProjectFileTypeService {
-
+    @Override
+    public Page<ProjectFileType> findPage(ProjectFileType projectFileType, int pageNumber, int pageSize){
+        Columns columns = Columns.create();
+        if (projectFileType.getName() != null) {
+            columns.like("name", "%" + projectFileType.getName() + "%");
+        }
+        if(projectFileType.getParentID()!=null){
+            columns.eq("parentID",projectFileType.getParentID());
+        }
+        return DAO.paginateByColumns(pageNumber, pageSize, columns.getList());
+    }
 }
