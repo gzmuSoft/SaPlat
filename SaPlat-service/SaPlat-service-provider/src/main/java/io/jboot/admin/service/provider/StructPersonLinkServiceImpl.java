8ebce573861efeb01d80db32e1f31226a0b703a4
rains <11945383+1782832653@users.noreply.github.com>
2018-08-04 20:25:53 +0800

    组织架构管理-修复表格表头显示 添加功能-查看我加入的组织架构功能

--- SaPlat-service/SaPlat-service-provider/src/main/java/io/jboot/admin/service/provider/StructPersonLinkServiceImpl.java
+++ SaPlat-service/SaPlat-service-provider/src/main/java/io/jboot/admin/service/provider/StructPersonLinkServiceImpl.java
@@ -1,6 +1,7 @@
 package io.jboot.admin.service.provider;
 
 import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
 import io.jboot.admin.service.api.StructPersonLinkService;
 import io.jboot.admin.service.entity.model.StructPersonLink;
 import io.jboot.aop.annotation.Bean;
@@ -12,6 +13,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Bean
 @Singleton
@@ -31,7 +33,7 @@ public class StructPersonLinkServiceImpl extends JbootServiceBase<StructPersonLi
     @Override
     public Map<String,Object> findByStructIdAndUsername(Long orgStructureId){
         List<StructPersonLink> list = Db.query("SELECT person.ID,person.personID,user.name,person.structID,person.createUserID,person.createTime,person.isEnable FROM `struct_person_link` as person,`sys_user` as user where person.personID = user.id and person.structID = ?",orgStructureId);
-        Map<String,Object> map = new HashMap<String, Object>();
+        Map<String,Object> map = new ConcurrentHashMap<String, Object>();
         map.put("code","0");
         map.put("msg","");
         map.put("count",list.size());
@@ -39,8 +41,16 @@ public class StructPersonLinkServiceImpl extends JbootServiceBase<StructPersonLi
         return map;
     }
     @Override
-    public List<StructPersonLink> findByPersonID(Long personID){
-        List<StructPersonLink> list = new LinkedList<StructPersonLink>();
-        return list;
+    public Map<String,Object> findStructureListByPersonID(Long personID){
+        List<Record> list = Db.find("select person.*,struct.name from `struct_person_link` person, `org_structure` struct where person.structID = struct.id and person.personID = ?",personID);
+        //构造layui表格数据格式
+        Map<String,Object> map = new ConcurrentHashMap<String,Object>();
+        map.put("code","0");
+        map.put("msg","");
+        map.put("count",list.size());
+        map.put("data",list);
+        return map;
     }
 }
+
+
