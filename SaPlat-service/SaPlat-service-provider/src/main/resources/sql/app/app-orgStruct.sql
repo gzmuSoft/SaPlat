5a697964459dcc3befac4d73fde9fce2610a3d68
rains <11945383+1782832653@users.noreply.github.com>
2018-08-13 21:20:49 +0800

    组织架构 部分功能以及分页完善

--- SaPlat-service/SaPlat-service-provider/src/main/resources/sql/app/app-orgStruct.sql
+++ SaPlat-service/SaPlat-service-provider/src/main/resources/sql/app/app-orgStruct.sql
@@ -2,11 +2,30 @@
   select
       person.*
       ,struct.name
-      from
-      `struct_person_link` person
-      , `org_structure` struct
-      where
+  from
+       `struct_person_link` as person
+      , `org_structure` as struct
+  where
       person.structID = struct.id
-      and person.personID = ?
-      order by person.createTime desc
+        and
+      person.personID = ?
+  order by person.createTime desc
+#end
+
+#sql("StructurePersonList")
+  select
+       person.ID
+      ,person.personID
+      ,user.name,person.structID
+      ,person.createUserID
+      ,person.createTime
+      ,person.isEnable
+  FROM
+       `struct_person_link` as person
+      ,`sys_user` as user
+  where
+      person.personID = user.userID
+        and
+      person.structID = ?
+  order by person.createTime desc
 #end
