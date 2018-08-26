2e6e3e1565fa799ed61c10dd7a853bf62dd2ea24
jywei2016 <254637046@qq.com>
2018-08-27 01:56:22 +0800

    1. 更加jquery为最新的3.3.1版本 2. 调整“用户注册”模块，增加鼠标移动到编辑框上给出相应的输入提示信息并完善相应的数据校验

--- SaPlat/src/main/java/io/jboot/admin/validator/app/person/PersonRegisterValidator.java
+++ SaPlat/src/main/java/io/jboot/admin/validator/app/person/PersonRegisterValidator.java
@@ -1,6 +1,7 @@
 package io.jboot.admin.validator.app.person;
 
 import com.jfinal.core.Controller;
+import com.jfinal.kit.StrKit;
 import io.jboot.admin.base.web.base.JsonValidator;
 
 /**
@@ -12,14 +13,18 @@ public class PersonRegisterValidator extends JsonValidator {
 
     @Override
     protected void validate(Controller c) {
-        validateString("user.name", 4, 16, "账号请在4-16位");
-        validateString("person.name", 2, 16, "姓名请在2位以上");
-        validateString("user.pwd", 6, 16, "密码长度请在6-16位");
-        validateString("person.sex",1,1,"请选择性别");
-        if (!c.getPara("user.pwd").equals(c.getPara("rePwd"))) {
-            addError("两次密码输入不一致");
+        validateString("user.name", 5, 25, "账号名称请在5-25位");
+        if(StrKit.notBlank(c.getPara("user.name")) && c.getPara("user.name").equals(c.getPara("user.pwd"))){
+            addError("密码不能和账号名称相同，请重新输入");
         }
-        validateMobile("person.phone", "电话号码格式不正确");
+        validatePassWord("user.pwd", "只能包含字母、数字以及标点符号（除空格），字母、数字和标点符号至少包含2种，密码长度为6-20位");
+        if (!c.getPara("user.pwd").equals(c.getPara("rePwd"))) {
+            addError("两次输入的密码不一致，请重新输入");
+        }
+        validateString("person.name", 2, 16, "姓名请在2-16位");
+        validateString("person.sex",1,1,"请选择性别");
+        validateMobile("person.phone", "手机格式不正确");
         validateEmail("user.email", "邮箱格式不正确");
+        validateString("user.email", 6, 60, "邮箱长度请在6-60位");
     }
 }
