c5396cab9bc4b5cf190a32769da2446673c9f431
jywei2016 <254637046@qq.com>
2018-08-30 20:37:25 +0800

    1. 修改用户登录时，用户名限制为5~25个字符改为4~25个字符

--- SaPlat/src/main/java/io/jboot/admin/validator/LoginValidator.java
+++ SaPlat/src/main/java/io/jboot/admin/validator/LoginValidator.java
@@ -12,7 +12,7 @@ public class LoginValidator extends JsonValidator {
 
     @Override
     protected void validate(Controller c) {
-        validateString("loginName", 5, 25, "用户名格式不正确，请在5-25位");
+        validateString("loginName", 4, 25, "用户名格式不正确，请在5-25位");
         validateString("password", 6, 16, "密码格式不正确，请在6-20位");
         validateString("capval", 4, 4, "验证码格式不正确");
         validateCaptcha("capval", "验证码不正确");
